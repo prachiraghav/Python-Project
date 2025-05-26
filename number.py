ffd2b8c528dd8c178e575d1ac4229afd738faa18
prachiraghav <51022502+prachiraghav@users.noreply.github.com>
2025-05-26 08:52:39 +0530

    Complete Random number game

---
+++ number.py
@@ -0,0 +1,27 @@
+# user to guess the number
+# take intput range
+# take guess of user
+#tell them how close they were the number
+# stop when the number is guessed
+
+
+import random 
+
+print("Welcome to Number Guessing Game!")
+print("Instructions:\n1. Choose the range greater than 0.\n2. Guess the number")
+num = int(input("Enter the top range number: "))
+
+if num > 0:
+    random_num = random.randint(0,num)
+else:
+    print("Invalid input!!")
+cnt = 0
+while True:
+    guess_num = int(input("Guess the number:"))
+    if guess_num ==  random_num:
+        print(f"🎉 Correct! You guessed the number in {cnt} try.")
+        break
+    else:
+        print("❌ Wrong guess. Try again.")
+        cnt += 1
+
