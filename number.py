aa15baadf2acce86265c456fc8b057703364f9d2
prachiraghav <51022502+prachiraghav@users.noreply.github.com>
2025-05-26 10:15:24 +0530

    updated

--- number.py
+++ number.py
@@ -1,10 +1,8 @@
 # user to guess the number
 # take intput range
 # take guess of user
-#tell them how close they were the number
+# tell them how close they were the number
 # stop when the number is guessed
-
-
 import random 
 
 print("Welcome to Number Guessing Game!")
@@ -17,11 +15,15 @@ else:
     print("Invalid input!!")
 cnt = 0
 while True:
+    cnt += 1
     guess_num = int(input("Guess the number:"))
     if guess_num ==  random_num:
         print(f"🎉 Correct! You guessed the number in {cnt} try.")
         break
+    elif guess_num > random_num:
+        print("Your guess was above the number.")
     else:
-        print("❌ Wrong guess. Try again.")
-        cnt += 1
+        print("Your guess was below the number.")
+    print("❌ Wrong guess. Try again.")
+        
 
